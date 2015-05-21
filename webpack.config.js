f1e2a7b156752243c8a5f842a794f49fd3d9b86b
joakin <joaquin@chimeces.com>
2015-05-21 20:46:11 +0100

    Improve webpack configuration and build

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,45 @@
 var path = require('path')
 var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var out = 'public'
+var node_modules = path.resolve(__dirname, 'node_modules')
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js')
 
 module.exports = {
   entry: {
-    app: './lib/index.js',
-    vendors: ['react']
+    app: [
+      'webpack-dev-server/client?http://0.0.0.0:3000',
+      'webpack/hot/only-dev-server',
+      './lib/index.js'
+    ]
+  },
+  resolve: {
+    alias: {
+      'react': pathToReact
+    }
   },
   output: {
     path: path.join(__dirname, out),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
+    noParse: [pathToReact],
     loaders: [
       { test: /\.(png|jpg)$/, loader: 'url?limit=25000' },
-      { test: /\.less$/, loader: 'style!css!autoprefixer!less' },
+      { test: /\.less$/, loader: ExtractTextPlugin.extract('style', 'css!autoprefixer!less') },
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendor.js')
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('style.css', { allChunks: true }),
+    new webpack.optimize.DedupePlugin()
   ],
   devServer: {
-    contentBase: out
+    port: 3000,
+    contentBase: out,
+    noInfo: true,
+    inline: true
   }
 }
