52f085b91e42109dee14e3db8c286b9d1a355db9
joakin <joaquin@chimeces.com>
2015-07-01 14:37:01 +0200

    Code format and remove commented reference code

--- src/webkipedia/ui/main_page.cljs
+++ src/webkipedia/ui/main_page.cljs
@@ -10,20 +10,4 @@
      {:class searching-class}
      [:h1.MainPage-header "Wikipedia"]
      [search-box {:query query :on-change println}]
-     (if loading [loading] nil)]
-    ))
-; export default component('MainPage', function (cursor) {
-;   let query = cursor.getIn(['search', 'query'])
-;   let searchingClass = Boolean(query) ? 'is-searching' : ''
-;   let onQueryChange = debounce(
-;     (query) => { console.log(`"${query}"`); return router.redirectTo(`search/${query}/`)},
-;     200, { leading: true, maxTime: 300 }
-;   )
-;   let results = cursor.getIn(['search', 'results', 'list'], null)
-;   let loading = query !== cursor.getIn(['search', 'results', 'query'], null)
-;   return div({ className: 'MainPage ' + searchingClass },
-;     h1({ className: 'MainPage-header' }, 'Wikipedia'),
-;     SearchBox({ query: query, onChange: onQueryChange }),
-;     loading ? Loading() : null,
-;     PageList({ items: results }))
-; })
+     (if loading [loading] nil)]))
