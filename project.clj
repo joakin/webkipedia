1a5b4d302a9f78e07b3004be148702025c26c02a
joakin <joaquin@chimeces.com>
2015-07-16 10:16:22 -0500

    Use webpack for css compiling
    * Also use post processor for vendor prefixes. Fixes iOS bugs with flex box for
    example
    * Now images can be local to the css files that import them

--- project.clj
+++ project.clj
@@ -43,7 +43,7 @@
   :figwheel {
              ;; :http-server-root "public" ;; default and assumes "resources"
              ;; :server-port 3449 ;; default
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
+             :css-dirs ["resources/public/"] ;; watch and update CSS
 
              ;; Start an nREPL server into the running figwheel process
              :nrepl-port 7888
