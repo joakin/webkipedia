aadd69bad8d8846a603336ffb47d838831888d3e
joakin <joaquin@chimeces.com>
2015-07-16 12:33:23 -0500

    PageList improvements
    * Thumbnail style
    * no duplicate keys in history
    * Dynamic font size on page list items depending on length of text

--- src/webkipedia/ui/page_list.cljs
+++ src/webkipedia/ui/page_list.cljs
@@ -2,16 +2,23 @@
   (:require [clojure.string :as s]
             [webkipedia.ui.thumbnail :as t]))
 
+(defn length-class [text]
+  (let [n (count text)
+        max-num 4]
+    (.min js/Math (.floor js/Math (/ n 10)) max-num)))
+
 (defn page-list-item [{:keys [title thumbnail] :as item}]
   (let [escaped-title (s/replace title #" " "_")
         url (str "#/wiki/" escaped-title)]
     [:a.PageListItem {:href url}
      [t/thumbnail thumbnail]
-     [:span.PageListItem-title title]]))
+     [:span.PageListItem-title
+      {:class (str "is-length-" (length-class escaped-title))}
+      title]]))
 
 (defn page-list [items]
   [:div.PageList
    (for [item items]
      [:div.PageList-item-wrapper
-      {:key (:title item)}
+      {:key (str (:title item) (:date item))}
       [page-list-item item]])])
